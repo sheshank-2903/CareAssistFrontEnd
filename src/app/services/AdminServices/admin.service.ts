788a62f3c3ce7e8778725a61faf4dd98c72f0049
Sheshank <Sheshanksharma2903@gmail.com>
2024-02-17 17:09:26 +0530

    inital installation and project structure

---
+++ src/app/services/AdminServices/admin.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Admin } from 'src/app/model/Admin';
+import { AuthRequest } from 'src/app/model/AuthRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private _http:HttpClient) { }
+
+  baseUrl:string = 'http://localhost:8080/api/v1/admin/'
+
+  addAdmin(body:Admin):Observable<Admin>{
+    return this._http.post<Admin>(this.baseUrl+"register",body)
+  }
+
+  updateAdmin(body:Admin):Observable<Admin>{
+    return this._http.put<Admin>(this.baseUrl+"update",body)
+  }
+
+  getAdminById(adminId:number):Observable<Admin>{
+    return this._http.get<Admin>(this.baseUrl+`get/${adminId}`)
+  }
+}
