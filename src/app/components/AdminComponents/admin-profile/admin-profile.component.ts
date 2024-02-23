519d5b2c99f2ebc9917de446d905e94ce24846c8
Sheshank <Sheshanksharma2903@gmail.com>
2024-02-23 11:23:19 +0530

    minor error resolved

--- src/app/components/AdminComponents/admin-profile/admin-profile.component.ts
+++ src/app/components/AdminComponents/admin-profile/admin-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-profile',
@@ -6,21 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-profile.component.css']
 })
 export class AdminProfileComponent {
-  
   editable:boolean=true;
 
-  constructor(){
+  updateForm !: FormGroup;
+  selectedGender: string="";
 
+  genderOptions = ['Male', 'Female'];
+
+  selectGender(gender: string): void {
+    this.selectedGender = gender;
+  }
+  constructor(private formBuilder: FormBuilder){
   }
 
+  ngOnInit(){
+    
+    this.updateForm=this.formBuilder.group({
+      adminName:['',[Validators.required,Validators.pattern('^[a-zA-Z ]{3,20}$')]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&./+]{8,}$')]],
+      confirm_password: ['', Validators.required] 
+    },{validator: this.passwordMatchValidator});
+  }
+
+
+  get getAdminForm(){
+  
+    return this.updateForm.controls;
+  }
+
+
   toggleEditable(){
-    if(this.editable){
-      this.editable=false;
-    }
-    else{
-      this.editable=true;
-    }
+    this.editable=!this.editable;
     // console.log("new value ", this.editable);
   }
 
+  onSubmit(){
+
+    if(this.updateForm.invalid){
+      return;
+  }
+  
+  alert('Form submitted successfully');
+  console.log(this.getAdminForm['companyName'].value);
+  
+  }
+
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirm_password = control.get('confirm_password')?.value;
+
+    if (password !== confirm_password) {
+      control.get('confirm_password')?.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
+  }
 }
