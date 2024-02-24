59986ed72e75f4c7625038d2f871b516f3860831
Sheshank <Sheshanksharma2903@gmail.com>
2024-02-24 12:04:13 +0530

    add admin form model created

--- src/app/components/AdminComponents/admin-home/admin-home.component.ts
+++ src/app/components/AdminComponents/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Admin } from 'src/app/model/Admin';
 import { AdminService } from 'src/app/services/AdminServices/admin.service';
 
@@ -8,10 +9,66 @@ import { AdminService } from 'src/app/services/AdminServices/admin.service';
   styleUrls: ['./admin-home.component.css']
 })
 export class AdminHomeComponent {
+  isAddAdminModelVisible: boolean=false;
+   addAdminForm !: FormGroup;
+
+toggleAddAdmin() {
+  let addModel=document.getElementById("addAdminFormModel");4
+  if(this.isAddAdminModelVisible){
+    addModel?.classList.remove("active");
+    this.isAddAdminModelVisible=false;
+  }
+  else{
+    addModel?.classList.add("active");
+    this.isAddAdminModelVisible=true;
+  }
+}
 
   adminList:Admin[]=[];
 
-  constructor(private adminService:AdminService){}
+  constructor(private adminService:AdminService, private formBuilder:FormBuilder){
+  
+  }
+
+  ngOnInit(){
+    
+    this. addAdminForm=this.formBuilder.group({
+      adminName:['',[Validators.required,Validators.pattern('^[a-zA-Z ]{3,20}$')]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&./+]{8,}$')]],
+      confirm_password: ['', Validators.required] 
+    },{validator: this.passwordMatchValidator});
+  }
+
+
+  get getAdminForm(){
+  
+    return this. addAdminForm.controls;
+  }
+
+  onSubmit(){
+
+    if(this. addAdminForm.invalid){
+      return;
+  }
+  
+  alert('Form submitted successfully');
+  console.log(this. addAdminForm);
+  this. addAdminForm.reset();
+  
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirm_password = control.get('confirm_password')?.value;
+
+    if (password !== confirm_password) {
+      control.get('confirm_password')?.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
+  }
 
 
 }
